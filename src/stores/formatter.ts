8cbf2fc935d5998585f699c4efcf637aa1edf59a
Tom <tomgrant1993@gmail.com>
2023-05-20 23:20:07 +1000

    basic styling

--- src/stores/formatter.ts
+++ src/stores/formatter.ts
@@ -40,19 +40,48 @@ export const useFormatterStore = defineStore('formatter', {
     },
     convertJSONToCSV(jsonString: string): string {
       try {
-        const jsonData: any[] = [JSON.parse(jsonString.replace(/\s/g, ''))];
-        const keys: string[] = Object.keys(jsonData[0]);
-        const csvHeader: string = keys.join(",");
-        const csvRows: string[] = jsonData.map((row: any) => {
-          return keys.map((key: string) => {
-            return row[key];
-          }).join(",");
-        });
+        const jsonData: any = JSON.parse(jsonString.replace(/\s/g, ''));
+        const csvRows: string[] = [];
+    
+        const processRow = (row: any, path: string = ''): void => {
+          Object.keys(row).forEach((key) => {
+            const value = row[key];
+    
+            if (Array.isArray(value)) {
+              value.forEach((item: any, index: number) => {
+                const newPath = path ? `${path}.${key}[${index}]` : `${key}[${index}]`;
+                processRow(item, newPath);
+              });
+            } else if (typeof value === 'object' && value !== null) {
+              const newPath = path ? `${path}.${key}` : key;
+              processRow(value, newPath);
+            } else {
+              const csvValue = typeof value === 'string' ? `"${value}"` : value;
+              csvRows.push(`${path ? `${path}.` : ''}${key},${csvValue}`);
+            }
+          });
+        };
+    
+        processRow(jsonData);
+    
+        const csvHeader: string = Object.keys(jsonData).join(",");
         return `${csvHeader}\n${csvRows.join("\n")}`;
       } catch (error) {
         console.error('Invalid JSON string:', error);
         return '';
       }
+    },
+    exportAsFile(text: string, filename: string) {
+      const blob = new Blob([text], { type: 'text/plain' });
+      const url = URL.createObjectURL(blob);
+      
+      const anchorElement = document.createElement('a');
+      anchorElement.href = url;
+      anchorElement.download = filename;
+      anchorElement.click();
+      
+      // Clean up the created URL object
+      URL.revokeObjectURL(url);
     }
   },
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,20 +8,41 @@ const { inputBody, outputBody } = store
 
 function handleUserInput(event: InputEvent): void {
   const userInput = (event.target as HTMLTextAreaElement).value;
+  // updateLineNumbers();
   store.inputBody = userInput;
   const formattedJson = store.convertStringToJSON(userInput);
   store.outputBody = formattedJson; // Parse and stringify the object
 }
 
 function handleConvert(type: string) {
-  if (type === 'csv') {
-    store.outputBody = store.convertJSONToCSV(store.inputBody);
+  switch (type) {
+    case 'csv':
+      store.outputBody = store.convertJSONToCSV(store.inputBody);
+      break;
+    case 'json':
+      store.outputBody = store.convertStringToJSON(store.inputBody);
+      break;
+    default:
+      text = "Looking forward to the Weekend";
   }
 }
 
+function updateLineNumbers() {
+  // Force Vue to update the computed property
+  this.$forceUpdate();
+};
+
 const formattedJsonString = computed(() => {
   return store.outputBody;
-})
+});
+
+const inputLineNumbers = computed(() => {
+  return store.inputBody.split('\n');
+});
+
+const outputLineNumbers = computed(() => {
+  return store.outputBody.split('\n');
+});
 
 </script>
 
@@ -30,18 +51,85 @@ const formattedJsonString = computed(() => {
     <div class="input">
       <h3>Input JSON</h3>
       <span class="input-error"></span>
-      <textarea id="userInput" v-model="inputBody" @input="handleUserInput"></textarea>
+      <div class="textarea-container">
+        <div class="line-numbers">
+          <div v-for="(line, index) in inputLineNumbers" :key="index">{{ index + 1 }}</div>
+        </div>
+        <textarea 
+          id="userInput"
+          v-model="inputBody" 
+          class="numbered"
+          @input="handleUserInput"
+          contenteditable
+        ></textarea>
+      </div>
     </div>
     <div class="controls">
-    <button @click="handleConvert('csv')">Convert to CSV</button>
+      <button @click="handleConvert('csv')">Convert to CSV</button>
+      <button @click="handleConvert('json')">Convert to json</button>
     </div>
     <div class="output">
-      <h3>Output JSON</h3>
-      <textarea :innerText="formattedJsonString"></textarea>
+      <h3>Output</h3>
+       <div class="textarea-container">
+        <div class="line-numbers">
+          <div v-for="(line, index) in outputLineNumbers" :key="index">{{ index + 1 }}</div>
+        </div>
+      <textarea 
+        class="numbered"
+        :innerText="formattedJsonString"
+      ></textarea>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+.container {
+  display: flex;
+  justify-content: space-between;
+  gap: 15px;
+}
+.input, .output {
+  flex: 2;
+}
+textarea.numbered {
+  padding-left: 35px;
+  padding-top: 10px;
+  border-color: #ccc;
+  display: block;
+  resize: none;
+  width: 100%;
+  box-sizing: border-box;
+  height: 500px;
+  max-height: 90vh;
+}
+.textarea-container {
+  position: relative;
+  overflow: hidden;
+}
+.line-numbers {
+  position: absolute;
+  color: #777;
+  top: 0;
+  left: 0;
+  width: 32px;
+  height: 100%;
+  padding: 0.5em;
+  font-family: monospace;
+  background-color: #f6f6f6;
+  border: 1px solid #ccc;
+  pointer-events: none;
+  user-select: none;
+  box-sizing: border-box;
+}
+.line-numbers:before {
+  content: attr(data-line);
+  display: block;
+}
+.controls {
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+}
 </style>
